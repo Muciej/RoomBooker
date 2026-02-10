a15a37845c71fc68a0e18452402d154538a8f21a
Maciek Józefkowicz <maciej.jozefkowicz@gmail.com>
2026-02-11 00:00:31 +0100

    Added temporary sql file with test data

---
+++ sql/test_db_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO administrators VALUES
+(0, 'Adam', 'Kowalski', 'tempPassword');
+
+INSERT INTO bookings VALUES
+(0, 1, '2026-02-10 14:30:00', '2026-02-10 16:30:00', 'Maciek Józefkowicz', TRUE),
+(1, 1, '2026-02-10 16:30:00', '2026-02-10 18:30:00', 'Suzie', TRUE),
+(2, 3, '2026-02-15 10:30:00', '2026-02-15 12:30:00', 'Kasztan Jakiśtam', TRUE),
+(3, 0, '2026-02-13 12:00:00', '2026-02-13 13:30:00', 'Lucy', FALSE);
+
+INSERT INTO classrooms VALUES
+(0, '', 10),
+(1, '', 11),
+(2, '', 12),
+(3, 'Widowiskowa', 20),
+(4, 'Aula', 30);
